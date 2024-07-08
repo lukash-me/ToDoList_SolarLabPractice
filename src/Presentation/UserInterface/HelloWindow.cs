9cc93ee0610fb2aa0ed5aa5b3bda530e462251fd
lukash <lukash.dept@gmail.com>
2024-07-08 15:45:14 +0300

    Добавлены Win формы

--- src/Presentation/UserInterface/HelloWindow.cs
+++ src/Presentation/UserInterface/HelloWindow.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace UserInterface
 {
-    public partial class Form1 : Form
+    public partial class HelloWindow : Form
     {
-        public Form1()
+        public HelloWindow()
         {
             InitializeComponent();
         }
