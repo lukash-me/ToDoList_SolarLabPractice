2a6dd5fa7ae79938908cb22e4b30a95ba1b39700
lukash <lukash.dept@gmail.com>
2024-07-08 15:25:00 +0300

    Созданы файлы классов

---
+++ src/Domain/Base/Tasks.cs
@@ -0,0 +1,13 @@
+﻿namespace Base
+{
+    public class Tasks
+    {
+        private int id {  get; set; }
+        private string name { get; set; }
+        private string group { get; set; }
+        private string priority { get; set; }
+        private string status { get; set; }
+        private float duration { get; set; }
+        private DateTime finishDate { get; set; }
+    }
+}
---
+++ src/Infrastructure/Infrastructure/DataProvider.cs
@@ -0,0 +1,15 @@
+﻿namespace Infrastructure
+{
+    internal class DataProvider : IDataProvider
+    {
+        public void loadData()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void saveData()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
